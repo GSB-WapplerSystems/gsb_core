42d0de4d0da3166d16b37118d4168a6e752e6b03
Kai Ole Hartwig <o.hartwig@moselwal.de>
2023-03-07 10:20:28 +0100

    chore: PSR-12

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,7 +19,6 @@ defined('TYPO3') or die('Access denied.');
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['crossOriginOpenerPolicy'] ??= false;
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['crossOriginResourcePolicy'] ??= false;
 
-
     /***************
      * Define TypoScript as content rendering template
      */
