1458df00b187555b38ae5faf04e0f936a340344b
Ole Hartwig <o.hartwig@moselwal.de>
2023-03-13 08:44:09 +0100

    feat: Import SiteConfig from Extension on load and Define Sitepackage in SiteConfig * Load TypoScript from Sitepacke configured in SiteCOnfig * Extend SiteConfig from Extensions * No More sys_templates needed - works if a sys_template exists * Extend any SiteConfig by Idtentifier from Extension * Choose Sitepacke gsb_temaplte or any with site in ExtKey

---
+++ Classes/Configuration/ExtendSiteConfigurationRegistry.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of TYPO3 CMS-extension "gsb_template".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ */
+
+namespace ITZBund\GsbTemplate\Configuration;
+
+use ITZBund\GsbTemplate\Configuration\Discovery\ExtendSiteConfigurationLocator;
+
+class ExtendSiteConfigurationRegistry
+{
+    /**
+     * @var array
+     */
+    protected array $extendSiteConfigurations = [];
+
+    public function __construct(ExtendSiteConfigurationLocator $locator)
+    {
+        $locator->locate($this);
+    }
+
+    public function add(string $identifier, string $extendSiteConfigurationPath): void
+    {
+        $this->extendSiteConfigurations[$identifier][] = $extendSiteConfigurationPath;
+    }
+
+    public function has(string $identifier): bool
+    {
+        return isset($this->extendSiteConfigurations[$identifier]);
+    }
+
+    public function get(string $identifier): array
+    {
+        if ($this->has($identifier)) {
+            return $this->extendSiteConfigurations[$identifier];
+        }
+        return $this->extendSiteConfigurations[$identifier];
+    }
+
+    public function getAll(): array
+    {
+        return $this->extendSiteConfigurations;
+    }
+}
---
+++ Classes/EventListener/ExtendsSiteConfigurationEvent.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of TYPO3 CMS-extension "gsb_template".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ */
+
+namespace ITZBund\GsbTemplate\EventListener;
+
+use ITZBund\GsbTemplate\Configuration\ExtendSiteConfigurationRegistry;
+use TYPO3\CMS\Core\Configuration\Event\SiteConfigurationLoadedEvent;
+use TYPO3\CMS\Core\Configuration\Loader\YamlFileLoader;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+final class ExtendsSiteConfigurationEvent
+{
+    private ExtendSiteConfigurationRegistry $registry;
+
+    public function __construct(ExtendSiteConfigurationRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function __invoke(SiteConfigurationLoadedEvent $event): void
+    {
+        $loader = GeneralUtility::makeInstance(YamlFileLoader::class);
+        $siteConfiguration = $event->getConfiguration();
+        foreach ($this->registry->get($event->getSiteIdentifier()) as $fileInfo) {
+            $configuration = $loader->load(GeneralUtility::fixWindowsFilePath((string)$fileInfo), YamlFileLoader::PROCESS_IMPORTS);
+            $siteConfiguration = array_merge($siteConfiguration, $configuration);
+            // Maybe better?
+            //ArrayUtility::mergeRecursiveWithOverrule($siteConfiguration, $configuration);
+        }
+        $event->setConfiguration($siteConfiguration);
+    }
+}
