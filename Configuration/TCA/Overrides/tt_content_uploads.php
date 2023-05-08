f5234fb0ffb038090e53ae56170a7101eb8ad8ea
Ole Hartwig <o.hartwig@moselwal.de>
2023-05-08 11:12:17 +0200

    feat: only allow pdf for download files refs: ITZBUNDPHP-503

---
+++ Configuration/TCA/Overrides/tt_content_uploads.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') || die();
+
+(static function (): void {
+    $GLOBALS['TCA']['tt_content']['types']['uploads']['columnsOverrides']['media']['config']['allowed'] = 'pdf';
+})();
