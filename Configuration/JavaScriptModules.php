be6b33c9f17e1261b5a10d38ad38631947d67d27
Patrick Schriner <patrick.schriner@diemedialen.de>
2023-11-12 13:37:40 +0100

    chore: Add german labels for language plugin

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -7,5 +7,6 @@ return [
     ],
     'imports' => [
         '@ckeditor/ckeditor5-language' => 'EXT:gsb_core/Resources/Public/CKEditor/JavaScript/plugin/ckeditor5-language.js',
+        '@ckeditor/ckeditor5-language-translation-de.js' => 'EXT:gsb_core/Resources/Public/CKEditor/JavaScript/plugin/ckeditor5-language-translation-de.js',
     ],
 ];
