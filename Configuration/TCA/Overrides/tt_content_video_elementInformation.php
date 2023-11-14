785b8154e496fdcacf1d3c7ad9bd90626a359860
Patrick Schriner <patrick.schriner@diemedialen.de>
2023-11-14 16:44:44 +0100

    chore: add example

---
+++ Configuration/TCA/Overrides/tt_content_video_elementInformation.php
@@ -0,0 +1,30 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['columns']['tx_video_caption']['config']['fieldInformation'] = [
+    'elementInformationText' => [
+        'renderType' => 'elementInformationText',
+        'options' => [
+            'texts' => [
+                [
+                    'text' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:tt_content.tx_video_caption',
+                    'bold' => true,
+                ],
+                [
+                    'text' => 'Simple text, just for test',
+                    'italic' => true,
+                ],
+                [
+                    'text' => 'Link text',
+                    'link' => 'https://www.google.com',
+                    'linkTarget' => '_self'
+                ],
+            ],
+        ],
+    ],
+];
+
+$GLOBALS['TCA']['tt_content']['types']['video']['infoButton'] = [
+    'title' => 'This is an info button',
+    'link' => 'https://google.com',
+    'linkTarget' => '_blank',
+];
