c167d910fc0b01b5913507a9ca227d2e2afa8cb8
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2023-11-29 18:26:28 +0100

    docs: add copirigth notice

--- Configuration/ContentSecurityPolicies.php
+++ Configuration/ContentSecurityPolicies.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
+ *
+ * (c) Kai Ole Hartwig <o.hartwig@moselwal.de> 2023
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Configuration\Features;
