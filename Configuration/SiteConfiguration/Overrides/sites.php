35f86bbc6d3301117b12b195d5cd9416214567f8
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2023-12-06 14:10:39 +0100

    docs: add copyright to configuration

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -1,11 +1,16 @@
 <?php
 
 /*
- * This file is part of TYPO3 CMS-based extension "gsb_core".
+ * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
+ *
+ * (c) Ole Hartwig <o.hartwig@moselwal.de> 2023
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
 
 $GLOBALS['SiteConfiguration']['site']['columns']['sitePackage'] = [
