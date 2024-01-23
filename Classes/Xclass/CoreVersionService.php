eff51abf67995333f616636efc02658b16fb2a1b
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2024-01-23 12:09:15 +0100

    fix: linting

--- Classes/Xclass/CoreVersionService.php
+++ Classes/Xclass/CoreVersionService.php
@@ -2,7 +2,4 @@
 
 namespace ITZBund\GsbCore\Xclass;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Install\CoreVersion\MaintenanceWindow;
-
 class CoreVersionService extends \TYPO3\CMS\Install\Service\CoreVersionService {}
