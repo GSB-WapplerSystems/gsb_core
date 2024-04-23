1be7380604c033ffc202e68c87be3d76c0c8321a
Patrick Schriner <patrick.schriner@diemedialen.de>
2024-04-23 09:20:53 +0200

    feat: Flush cache on site config change
    As a followup to ITZBUNDPHP-3088 we flush the 'pages' cache group when
    site config changes are written. This should spread configuration changes
    that influence the frontend as long as the caches are shared.
    
    Refs: #ITZBUNDPHP-3089

---
+++ Classes/EventListener/FlushCacheOnSiteConfigurationChange.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+/*
+  * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
+  *
+  * Copyright (C) 2024 Bundesrepublik Deutschland, vertreten durch das
+  * BMI/ITZBund. Author: Patrick Schriner
+  *
+  * It is free software; you can redistribute it and/or modify it under
+  * the terms of the GNU General Public License, either version 2
+  * of the License, or any later version.
+  *
+  * For the full copyright and license information, please read the
+  * LICENSE file that was distributed with this source code.
+  */
+
+namespace ITZBund\GsbCore\EventListener;
+
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Configuration\Event\SiteConfigurationBeforeWriteEvent;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * This event listener is used to trigger cache flushes when the site configuration has changed
+ */
+final class FlushCacheOnSiteConfigurationChange
+{
+    public function __invoke(SiteConfigurationBeforeWriteEvent $siteConfigurationBeforeWriteEvent): void
+    {
+        // we play "stupid" and simply clear all caches in the pages group
+        $cacheManager = GeneralUtility::makeInstance(CacheManager::class);
+        $cacheManager->flushCachesInGroup('pages');
+    }
+}
