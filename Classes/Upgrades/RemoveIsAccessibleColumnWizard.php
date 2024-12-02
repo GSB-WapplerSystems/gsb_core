1d15b43fa211d4b44cfa50be1ea1ee2222cb7e70
Marco Luig <marco.luig@digitas.com>
2024-12-02 09:28:46 +0100

    fix: added copyright header
    Refs: ITZBUNDPHP-3944

--- Classes/Upgrades/RemoveIsAccessibleColumnWizard.php
+++ Classes/Upgrades/RemoveIsAccessibleColumnWizard.php
@@ -1,5 +1,24 @@
 <?php
+
+// SPDX-FileCopyrightText: 2024 Bundesrepublik Deutschland, vertreten durch das BMI/ITZBund
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 declare(strict_types=1);
+
+/*
+  * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
+  *
+  * Copyright (C) 2023 - 2024 Bundesrepublik Deutschland, vertreten durch das
+  * BMI/ITZBund. Author: Marco Luig
+  *
+  * It is free software; you can redistribute it and/or modify it under
+  * the terms of the GNU General Public License, either version 3
+  * of the License, or any later version.
+  *
+  * For the full copyright and license information, please read the
+  * LICENSE file that was distributed with this source code.
+  */
 
 namespace ITZBund\GsbCore\Upgrades;
 
