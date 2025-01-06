d505a4f1405bf36697e2d8ffc325ceab8ea2a64f
Christian Rath-Ulrich <christian.rath-ulrich@digitas.com>
2025-01-06 16:25:39 +0100

    feat: add events and view helper for trademark condition
    Refs: ITZBUNDPHP-4320

---
+++ Classes/ViewHelpers/TrademarkViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+// SPDX-FileCopyrightText: 2025 Bundesrepublik Deutschland, vertreten durch das BMI/ITZBund
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/*
+  * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
+  *
+  * Copyright (C) 2025 Bundesrepublik Deutschland, vertreten durch das
+  * BMI/ITZBund. Author: Christian Rath-Ulrich
+  *
+  * It is free software; you can redistribute it and/or modify it under
+  * the terms of the GNU General Public License, either version 3
+  * of the License, or any later version.
+  *
+  * For the full copyright and license information, please read the
+  * LICENSE file that was distributed with this source code.
+  */
+
+declare(strict_types=1);
+
+namespace ITZBund\GsbCore\ViewHelpers;
+
+use ITZBund\GsbCore\Event\GetTrademarkLogoEvent;
+use ITZBund\GsbCore\Event\GetTrademarkTextEvent;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class TrademarkViewHelper extends AbstractViewHelper
+{
+    // @var bool
+    protected $escapeOutput = false;
+
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('type', 'string', 'The name of the asset', true);
+    }
+
+    public function render(): string
+    {
+        $assetType = $this->arguments['type'];
+
+        $eventDispatcher = GeneralUtility::makeInstance(EventDispatcher::class);
+        switch ($assetType) {
+            case 'logo':
+                $event = new GetTrademarkLogoEvent();
+                $eventDispatcher->dispatch($event);
+                return $event->getValue();
+            case 'text':
+                $event = new GetTrademarkTextEvent();
+                $eventDispatcher->dispatch($event);
+                return $event->getValue();
+        }
+        return '';
+    }
+}
