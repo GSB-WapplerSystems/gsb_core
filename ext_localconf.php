1b3a7d4a0e5696b93bb47d3e9acf8a8dd3b36870
Patrick Schriner <patrick.schriner@diemedialen.de>
2025-01-07 17:10:37 +0100

    fix: prevent identical email path override
    Refs: NO-JIRA

--- ext_localconf.php
+++ ext_localconf.php
@@ -59,7 +59,7 @@ defined('TYPO3') or die('Access denied.');
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['ITZBUNDPHP-1615'] ??= false;
     if (GeneralUtility::makeInstance(Features::class)->isFeatureEnabled('ITZBUNDPHP-1615')) {
-        $GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths']['20'] = 'EXT:gsb_core/Resources/Private/Layouts/Email/';
+        $GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths']['100'] = 'EXT:gsb_core/Resources/Private/Layouts/Email/';
     }
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['ITZBUNDPHP-3327'] ??= false;
