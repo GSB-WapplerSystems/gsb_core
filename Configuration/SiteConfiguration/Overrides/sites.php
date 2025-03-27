8f842581101f7e851b8001461981121692c347a4
Christian Rath-Ulrich <christian.rath-ulrich@digitas.com>
2025-03-27 10:37:05 +0100

    feat: rename variables to better reflect use case
    Refs: ITZBUNDPHP-4518

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -23,7 +23,7 @@ use TYPO3\CMS\Core\Configuration\Features;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',
-    ,--div--;GSB,sitePackage, show-copyright, navType, google_site_verification, copyright, sign-language, simple-language, --palette--;;logos, --palette--;;favicon, --palette--;;color,--palette--;;color-general,--palette--;;fonts
+    ,--div--;GSB,sitePackage, show-copyright, navType, google_site_verification, copyright, sign-language-page, simple-language-page, --palette--;;logos, --palette--;;favicon, --palette--;;color,--palette--;;color-general,--palette--;;fonts
 ';
 
 $GLOBALS['SiteConfiguration']['site']['palettes']['favicon']['label'] = 'Favicons';
@@ -552,9 +552,9 @@ if (GeneralUtility::makeInstance(Features::class)->isFeatureEnabled('ITZBUNDPHP-
             ],
     ];
 
-    $GLOBALS['SiteConfiguration']['site']['columns']['simple-language'] = [
-        'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.simple-language.label',
-        'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.simple-language.description',
+    $GLOBALS['SiteConfiguration']['site']['columns']['simple-language-page'] = [
+        'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.simple-language-page.label',
+        'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.simple-language-page.description',
         'config' =>
             [
                 'type' => 'link',
@@ -564,9 +564,9 @@ if (GeneralUtility::makeInstance(Features::class)->isFeatureEnabled('ITZBUNDPHP-
             ],
     ];
 
-    $GLOBALS['SiteConfiguration']['site']['columns']['sign-language'] = [
-        'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sign-language.label',
-        'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sign-language.description',
+    $GLOBALS['SiteConfiguration']['site']['columns']['sign-language-page'] = [
+        'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sign-language-page.label',
+        'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sign-language-page.description',
         'config' =>
             [
                 'type' => 'link',
