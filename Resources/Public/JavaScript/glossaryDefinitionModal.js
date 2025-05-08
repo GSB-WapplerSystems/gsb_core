2758e3a7ab9beb1495bf723bfa028bbeea978c08
Thorsten Müller <thorsten.mueller@adesso.de>
2025-05-08 16:17:07 +0200

    feat(glossary-modal): add footer link for modal
    Refs: ITZBUNDPHP-2076

--- Resources/Public/JavaScript/glossaryDefinitionModal.js
+++ Resources/Public/JavaScript/glossaryDefinitionModal.js
@@ -6,7 +6,11 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('dfn').forEach(function (el) {
+    el.querySelector('a').addEventListener('click', event => {
+      event.preventDefault();
+    });
     el.addEventListener('click', function () {
+      document.getElementById('glossary-definition-modal-link').href = el.querySelector('a').href || "#";
       document.getElementById('glossary-definition-modal-title').innerHTML = el.dataset.glossaryTitle || "";
       document.getElementById('glossary-definition-modal-body').innerHTML = el.dataset.glossaryText || "";
     });
