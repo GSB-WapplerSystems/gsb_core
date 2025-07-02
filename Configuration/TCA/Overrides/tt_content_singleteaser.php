2f8585316b40764c855e68bf40736f9f74b42789
Christian Rath-Ulrich <crathulrich@network-publishing.de>
2025-07-02 13:09:18 +0200

    feat: remove featureflag ITZBUNDPHP-2328
    Refs: ITZBUNDPHP-5239

--- Configuration/TCA/Overrides/tt_content_singleteaser.php
+++ Configuration/TCA/Overrides/tt_content_singleteaser.php
@@ -102,14 +102,12 @@ defined('TYPO3') || die();
 
     $GLOBALS['TCA']['tt_content']['types']['gsb_singleteaser']['previewRenderer'] = SingleteaserPreviewRenderer::class;
 
-    if (GeneralUtility::makeInstance(Features::class)->isFeatureEnabled('ITZBUNDPHP-2328')) {
-        ExtensionManagementUtility::addToAllTCAtypes(
-            'tt_content',
-            '--palette--;;category_date_override',
-            'gsb_singleteaser',
-            'after:header',
-        );
-    }
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        '--palette--;;category_date_override',
+        'gsb_singleteaser',
+        'after:header',
+    );
 
     if (GeneralUtility::makeInstance(Features::class)->isFeatureEnabled('ITZBUNDPHP-3969')) {
         ExtensionManagementUtility::addToAllTCAtypes(
