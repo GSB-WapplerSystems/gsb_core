b6359c7a8cee80e298b234f25fb4e49747c4ae80
Christian Rath-Ulrich <crathulrich@network-publishing.de>
2025-09-12 13:40:10 +0200

    chore: remove unused config
    Refs: NO-JIRA

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -20,54 +20,12 @@
 
 use ITZBund\GsbCore\Configuration\PackageHelper;
 
-$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',
-    ,--div--;GSB,sitePackage,  --palette--;;, -, --palette--;;color,--palette--;;color-general,--palette--;;fonts
-';
 
 
 
 $GLOBALS['SiteConfiguration']['site']['palettes']['logos']['label'] = 'Logos';
 $GLOBALS['SiteConfiguration']['site']['palettes']['logos']['description'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sitepackage.logos.palette.description';
 $GLOBALS['SiteConfiguration']['site']['palettes']['logos']['showitem'] = 'logo-complete-toggle,second-logo-complete-toggle, logo-text, logo-complete-big, logo-complete-small, second-logo, second-logo-alt, second-logo-link';
-
-$GLOBALS['SiteConfiguration']['site']['palettes']['color']['label'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sitepackage.palette.color.label';
-$GLOBALS['SiteConfiguration']['site']['palettes']['color']['description'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sitepackage.palette.color.description';
-$GLOBALS['SiteConfiguration']['site']['palettes']['color']['showitem'] = 'color_1, label_color_1, color_2, label_color_2, color_3, label_color_3, color_4, label_color_4, color_5, label_color_5, color_6, label_color_6';
-
-$GLOBALS['SiteConfiguration']['site']['palettes']['color-general']['label'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sitepackage.palette.color-general.label';
-$GLOBALS['SiteConfiguration']['site']['palettes']['color-general']['description'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.sitepackage.palette.color-general.description';
-$GLOBALS['SiteConfiguration']['site']['palettes']['color-general']['showitem'] = 'color_primary,color_secondary,color_secondary_rgba,color_tertiary,color_quaternary';
-
-
-
-
-
-
-
-// ITZBUNDPHP-2869 Logo Textmarke
-$GLOBALS['SiteConfiguration']['site']['columns']['logo-text'] = [
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.logo-text',
-    'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.description.logo-text',
-    'displayCond' => 'FIELD:logo-complete-toggle:REQ:false',
-
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-        ],
-];
-
-//ITZBUNDPHP-2903 Logo komplett austauschen
-$GLOBALS['SiteConfiguration']['site']['columns']['logo-complete-toggle'] = [
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.logo-complete-toggle',
-    'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.description.logo-complete-toggle',
-    'onChange' => 'reload',
-    'config' => [
-        'renderType' => 'checkboxToggle',
-        'type' => 'check',
-        'default' => 0,
-    ],
-];
 
 $GLOBALS['SiteConfiguration']['site']['columns']['logo-complete-big'] = [
     'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.logo-complete-big',
@@ -133,13 +91,6 @@ $GLOBALS['SiteConfiguration']['site']['columns']['second-logo-link'] = [
         ],
 ];
 
-
-
-
-
-
-
-
 $GLOBALS['SiteConfiguration']['site']['columns']['initiative-text-toggle'] = [
     'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.initiative-text-toggle.label',
     'displayCond' => 'FIELD:second-logo-complete-toggle:REQ:true',
@@ -170,14 +121,11 @@ $GLOBALS['SiteConfiguration']['site']['palettes']['logos']['showitem'] .= ', ini
 
 // ITZBUNDPHP-3288 Localizable site settings
 $localizableKeys = [
-    'logo-complete-toggle',
-    'second-logo-complete-toggle',
     'logo-complete-big',
     'logo-complete-small',
     'second-logo',
     'second-logo-alt',
     'second-logo-link',
-    'initiative-text-toggle',
     'initiative-text',
     'logo-text',
 ];
@@ -202,152 +150,3 @@ $GLOBALS['SiteConfiguration']['site_language']['palettes']['localized-logos-and-
 ];
 
 $GLOBALS['SiteConfiguration']['site_language']['types']['1']['showitem'] .= ',--palette--;;localized-logos-and-copyright';
-
-$GLOBALS['SiteConfiguration']['site']['columns']['display-brand-topline'] = [
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.display-brand-topline.label',
-    'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.display-brand-topline.description',
-    'config' => [
-        'renderType' => 'checkboxToggle',
-        'type' => 'check',
-        'default' => 0,
-    ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['palettes']['logos']['showitem'] .= ', display-brand-topline';
-
-$GLOBALS['SiteConfiguration']['site']['palettes']['fonts']['label'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.fonts.palette.label';
-$GLOBALS['SiteConfiguration']['site']['palettes']['fonts']['description'] = 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.fonts.palette.description';
-$GLOBALS['SiteConfiguration']['site']['palettes']['fonts']['showitem'] = 'font-switch, --linebreak--, font-sans-name, --linebreak--, font-sans, font-sans-italic, --linebreak--, font-sans-medium, --linebreak--, font-sans-bold, font-sans-bold-italic, --linebreak--, font-serif-name, --linebreak--, font-serif, font-serif-italic';
-$GLOBALS['SiteConfiguration']['site']['columns']['font-switch'] = [
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-switch.label',
-    'description' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-switch.description',
-    'config' => [
-        'renderType' => 'checkboxToggle',
-        'type' => 'check',
-        'default' => 0,
-    ],
-    'onChange' => 'reload',
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans-name'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans-name.label',
-    'config' => [
-        'type' => 'input',
-        'size' => 50,
-    ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans-italic'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans-italic.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans-medium'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans-medium.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans-bold'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans-bold.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-sans-bold-italic'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-sans-bold-italic.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-serif-name'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-serif-name.label',
-    'config' => [
-        'type' => 'input',
-        'size' => 50,
-    ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-serif'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-serif.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
-
-$GLOBALS['SiteConfiguration']['site']['columns']['font-serif-italic'] = [
-    'displayCond' => 'FIELD:font-switch:REQ:true',
-    'label' => 'LLL:EXT:gsb_core/Resources/Private/Language/locallang_db.xlf:page.configuration.font-serif-italic.label',
-    'config' =>
-        [
-            'type' => 'link',
-            'allowedTypes' => ['file'],
-            'size' => 50,
-        ],
-    'appearance' =>
-        [
-            'allowedExtensions' => ['woff2'],
-        ],
-];
