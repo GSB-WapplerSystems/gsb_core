209b667e878fbc76df6f717098746e3b6239d665
Christian Rath-Ulrich <crathulrich@network-publishing.de>
2025-10-22 13:37:38 +0200

    feat: update command classes to use attributes for Symfony console commands and remove legacy service definitions
    Refs: ITZBUNDPHP-5908

--- Classes/Command/TriggerSynchronizeConfigurationCommand.php
+++ Classes/Command/TriggerSynchronizeConfigurationCommand.php
@@ -8,6 +8,7 @@ namespace ITZBund\GsbCore\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
