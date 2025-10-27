612ce417bc94d760801fc584629d2f624fe1f383
Thorsten Müller <thorsten.mueller@adesso.de>
2025-10-27 16:34:25 +0100

    fix(composer): fix composer dependencies
    Refs: NO-JIRA

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -6,7 +6,7 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
-    // Unit
+    // UnitTests
     ->ignoreErrorsOnPackage('phpunit/phpunit', [ErrorType::SHADOW_DEPENDENCY])
     ->ignoreErrorsOnPackage('typo3/testing-framework', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     // GSB-Core Base
@@ -43,4 +43,21 @@ return (new Configuration())
     ->ignoreErrorsOnPackage('typo3/cms-viewpage', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('typo3/cms-workspaces', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('wapplersystems/multisite-belogin', [ErrorType::UNUSED_DEPENDENCY])
-    ->ignoreErrorsOnPackage('wapplersystems/save_and_close', [ErrorType::UNUSED_DEPENDENCY]);
+    ->ignoreErrorsOnPackage('wapplersystems/save_and_close', [ErrorType::UNUSED_DEPENDENCY])
+    // included by bacon/bacon-qr-code, which is included by typo3/cms-core
+    ->ignoreErrorsOnPackage('dasprid/enum', [ErrorType::SHADOW_DEPENDENCY])
+    // included by typo3/cms-core
+    ->ignoreErrorsOnPackage('psr/event-dispatcher', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-factory', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-message', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-server-handler', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/http-server-middleware', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/log', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/console', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/dependency-injection', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/finder', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/yaml', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('typo3fluid/fluid', [ErrorType::SHADOW_DEPENDENCY])
+    // included by symfony/serializer-pack
+    ->ignoreErrorsOnPackage('symfony/serializer', [ErrorType::SHADOW_DEPENDENCY])
+;
