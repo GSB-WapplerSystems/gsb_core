a443e9c3bb4b11238ec17ba4780b800915b7aa73
Thorsten Müller <thorsten.mueller@adesso.de>
2025-11-12 11:22:21 +0100

    feat(menu): add menu dataprocessor for improved performance
    Refs: ITZBUNDPHP-6082

--- ext_localconf.php
+++ ext_localconf.php
@@ -170,4 +170,11 @@ defined('TYPO3') or die('Access denied.');
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['f'][] = 'ITZBund\\GsbCore\\Fluid\\ViewHelpers';
 
+    // Configure caching framework
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['gsb_core_menu'] = [
+        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+        'backend' => \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
+        'options' => ['defaultLifetime' => 2592000], // 30 days
+        'groups' => ['pages'],
+    ];
 })();
