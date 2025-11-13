9294ca02c7b5f1a0b3aa4804b5e6142e9f56b119
Thorsten Müller <thorsten.mueller@adesso.de>
2025-11-13 15:34:43 +0100

    fix(menu): fix menu cache for logged in fe_user
    Refs: ITZBUNDPHP-6079

--- Classes/DataProcessing/CachedMenuProcessor.php
+++ Classes/DataProcessing/CachedMenuProcessor.php
@@ -1,14 +1,14 @@
 <?php
 
-// SPDX-FileCopyrightText: 2024 Bundesrepublik Deutschland, vertreten durch das BMI/ITZBund
+// SPDX-FileCopyrightText: 2025 Bundesrepublik Deutschland, vertreten durch das BMI/ITZBund
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 /*
   * This file is part of the package itzbund/gsb-core of the GSB 11 Project by ITZBund.
   *
-  * Copyright (C) 2024 Bundesrepublik Deutschland, vertreten durch das
-  * BMI/ITZBund. Author: Christian Rath-Ulrich
+  * Copyright (C) 2025 Bundesrepublik Deutschland, vertreten durch das
+  * BMI/ITZBund. Author: Thorsten Müller
   *
   * It is free software; you can redistribute it and/or modify it under
   * the terms of the GNU General Public License, either version 3
@@ -23,7 +23,8 @@ namespace ITZBund\GsbCore\DataProcessing;
 use Doctrine\DBAL\Exception;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Context\UserAspect;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 use TYPO3\CMS\Frontend\DataProcessing\MenuProcessor;
@@ -35,10 +36,19 @@ class CachedMenuProcessor implements DataProcessorInterface
     private string $configLevels = '';
     private string $configRootPageId = '';
     private string $configBreadcrumbAs = '';
+    private UserAspect $userAspect;
+    private FrontendInterface $cache;
+
+    public function __construct(
+        private readonly Context $context,
+        private readonly CacheManager $cacheManager,
+        private readonly MenuProcessor $menuProcessor,
+    ) {
+        $this->userAspect = $this->context->getAspect('frontend.user');
+        $this->cache = $this->cacheManager->getCache('gsb_core_menu');
+    }
+
     /**
-     * @phpstan-ignore-next-line
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     *
      * @param mixed[] $contentObjectConfiguration
      * @param mixed[] $processorConfiguration
      * @param mixed[] $processedData
@@ -53,20 +63,30 @@ class CachedMenuProcessor implements DataProcessorInterface
         array $processorConfiguration,
         array $processedData
     ): array {
-        $this->configAs = $processorConfiguration['as'] ?? 'menuMain';
-        $this->configIncludeSpacer = $processorConfiguration['includeSpacer'] ?? '1';
-        $this->configLevels = $processorConfiguration['levels'] ?? '4';
-        $this->configRootPageId = $processorConfiguration['rootPageId'] ?? '0';
-        $this->configBreadcrumbAs = $processorConfiguration['breadcrumbAs'] ?? 'breadcrumbMenu';
+        $this->setConfig($processorConfiguration);
 
         if ($this->configRootPageId === '0') {
             return $processedData;
         }
 
-        $processedData = $this->getCachedData($cObj, $contentObjectConfiguration, $processedData);
-        $processedData = $this->setBreadcrumb($cObj, $contentObjectConfiguration, $processedData);
+        $processedData = $this->processMenuCache($cObj, $contentObjectConfiguration, $processedData);
+        $processedData = $this->processBreadcrumb($cObj, $contentObjectConfiguration, $processedData);
+        $rootLine = $this->getRootLineByBreadcrumb($processedData[$this->configBreadcrumbAs]);
+        $processedData[$this->configAs] = $this->processCachedMenuDataIterative($processedData[$this->configAs], $rootLine);
 
         return $processedData;
+    }
+
+    /**
+     * @param mixed[] $processorConfiguration
+     */
+    private function setConfig(array $processorConfiguration): void
+    {
+        $this->configAs = $processorConfiguration['as'] ?? 'menuMain';
+        $this->configIncludeSpacer = $processorConfiguration['includeSpacer'] ?? '1';
+        $this->configLevels = $processorConfiguration['levels'] ?? '4';
+        $this->configRootPageId = $processorConfiguration['rootPageId'] ?? '0';
+        $this->configBreadcrumbAs = $processorConfiguration['breadcrumbAs'] ?? 'breadcrumbMenu';
     }
 
     /**
@@ -76,7 +96,7 @@ class CachedMenuProcessor implements DataProcessorInterface
      *
      * @return mixed[]
      */
-    private function getCachedData(
+    private function processMenuCache(
         ContentObjectRenderer $cObj,
         array $contentObjectConfiguration,
         array $processedData
@@ -85,30 +105,23 @@ class CachedMenuProcessor implements DataProcessorInterface
             'as' => $this->configAs,
             'includeSpacer' => $this->configIncludeSpacer,
             'levels' => $this->configLevels,
+            'showAccessRestrictedPages' => '1',
             'special' => 'directory',
             'special.' => [
                 'value' => $this->configRootPageId,
             ],
         ];
 
-        $cache = $this->getCache();
-
-        if ($cache->has($this->configRootPageId)) {
-            $processedData[$this->configAs] = $cache->get($this->configRootPageId);
+        if ($this->cache->has($this->configRootPageId)) {
+            $processedData[$this->configAs] = $this->cache->get($this->configRootPageId);
 
             return $processedData;
         }
 
-        $menuProcessor = GeneralUtility::makeInstance(MenuProcessor::class);
-        $processedData = $menuProcessor->process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
-        $cache->set($this->configRootPageId, $processedData[$this->configAs]);
+        $processedData = $this->menuProcessor->process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
+        $this->cache->set($this->configRootPageId, $processedData[$this->configAs]);
 
         return $processedData;
-    }
-
-    private function getCache(): FrontendInterface
-    {
-        return GeneralUtility::makeInstance(CacheManager::class)->getCache('gsb_core_menu');
     }
 
     /**
@@ -118,44 +131,68 @@ class CachedMenuProcessor implements DataProcessorInterface
      *
      * @return mixed[]
      */
-    private function setBreadcrumb(
+    private function processBreadcrumb(
         ContentObjectRenderer $cObj,
         array $contentObjectConfiguration,
         array $processedData
     ): array {
-        $menuProcessor = GeneralUtility::makeInstance(MenuProcessor::class);
-        $rootLineConfig = ['as' => $this->configBreadcrumbAs, 'special' => 'rootline'];
-        $processedData = $menuProcessor->process($cObj, $contentObjectConfiguration, $rootLineConfig, $processedData);
-        $breadcrumb = [];
+        return $this->menuProcessor->process(
+            $cObj,
+            $contentObjectConfiguration,
+            ['as' => $this->configBreadcrumbAs, 'special' => 'rootline'],
+            $processedData
+        );
+    }
 
-        foreach ($processedData[$this->configBreadcrumbAs] as $item) {
+    /**
+     * @param mixed[] $processedBreadcrumbData
+     *
+     * @return array<int, array<string, int>>
+     */
+    private function getRootLineByBreadcrumb(array $processedBreadcrumbData): array
+    {
+        $rootLine = [];
+
+        foreach ($processedBreadcrumbData as $item) {
             $uid = $item['data']['uid'];
-            $breadcrumb[$uid]['active'] = $item['active'];
-            $breadcrumb[$uid]['current'] = $item['current'];
+            $rootLine[$uid]['active'] = $item['active'];
+            $rootLine[$uid]['current'] = $item['current'];
         }
 
-        $processedData[$this->configAs] = $this->iterateCurrentActiveMenu($processedData[$this->configAs], $breadcrumb);
-
-        return $processedData;
+        return $rootLine;
     }
 
     /**
      * @param mixed[] $processedMenuData
-     * @param mixed[] $breadcrumb
+     * @param array<int, array<string, int>> $rootLine
      *
      * @return mixed[]
      */
-    private function iterateCurrentActiveMenu(array $processedMenuData, array $breadcrumb): array
+    private function processCachedMenuDataIterative(array $processedMenuData, array $rootLine): array
     {
         foreach ($processedMenuData as $key => $menu) {
-            $processedMenuData[$key]['active'] = (int)($breadcrumb[$menu['data']['uid']]['active'] ?? 0);
-            $processedMenuData[$key]['current'] = (int)($breadcrumb[$menu['data']['uid']]['current'] ?? 0);
+            if (!$this->isMenuAccessibleForUser((int)$menu['data']['fe_group'])) {
+                unset($processedMenuData[$key]);
+
+                continue;
+            }
+
+            $menuUid = $menu['data']['uid'];
+            $processedMenuData[$key]['active'] = (int)($rootLine[$menuUid]['active'] ?? 0);
+            $processedMenuData[$key]['current'] = (int)($rootLine[$menuUid]['current'] ?? 0);
 
             if (isset($menu['children'])) {
-                $processedMenuData[$key]['children'] = $this->iterateCurrentActiveMenu($menu['children'], $breadcrumb);
+                $processedMenuData[$key]['children'] = $this->processCachedMenuDataIterative($menu['children'], $rootLine);
             }
         }
 
         return $processedMenuData;
+    }
+
+    private function isMenuAccessibleForUser(int $allowedGroupId): bool
+    {
+        $currentUserGroups = $this->userAspect->getGroupIds();
+
+        return in_array($allowedGroupId, $currentUserGroups, true);
     }
 }
