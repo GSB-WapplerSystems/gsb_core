d34200614eb9304b113b73438371410268f2734c
Christian Rath-Ulrich <crathulrich@network-publishing.de>
2025-11-20 17:46:49 +0100

    feat(gsb_core): add 'config.navType' to the list of constants to remove in RemoveOldConstantsFromTypoScript class
    Refs: NO-JIRA

--- Classes/Upgrades/RemoveOldConstantsFromTypoScript.php
+++ Classes/Upgrades/RemoveOldConstantsFromTypoScript.php
@@ -46,6 +46,7 @@ class RemoveOldConstantsFromTypoScript extends AbstractMoveConfigurationToSettin
      */
     protected const CONSTANT_KEYS_TO_REMOVE = [
         'config.debug',
+		'config.navType',
         'config.admPanel',
         'config.no_cache',
         'config.pids.Search',
