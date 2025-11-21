ae0c68657a9e1a4be1cfef98e9c5c30210e467d6
Thorsten Müller <thorsten.mueller@adesso.de>
2025-11-21 17:03:29 +0100

    fix(menu-cache): add language-dependent for menu cache
    Refs: NO-JIRA

--- Classes/DataProcessing/CachedMenuProcessor.php
+++ Classes/DataProcessing/CachedMenuProcessor.php
@@ -112,16 +112,23 @@ class CachedMenuProcessor implements DataProcessorInterface
             ],
         ];
 
-        if ($this->cache->has($this->configRootPageId)) {
+        $langCode = $this->getCurrentLangCode($cObj);
+
+        if ($this->cache->has($this->configRootPageId . '-' . $langCode)) {
             $processedData[$this->configAs] = $this->cache->get($this->configRootPageId);
 
             return $processedData;
         }
 
         $processedData = $this->menuProcessor->process($cObj, $contentObjectConfiguration, $processorConfiguration, $processedData);
-        $this->cache->set($this->configRootPageId, $processedData[$this->configAs]);
+        $this->cache->set($this->configRootPageId . '-' . $langCode, $processedData[$this->configAs]);
 
         return $processedData;
+    }
+
+    private function getCurrentLangCode(ContentObjectRenderer $cObj): string
+    {
+        return $cObj->getRequest()?->getAttribute('language')?->getLocale()?->getLanguageCode() ?? 'default';
     }
 
     /**
