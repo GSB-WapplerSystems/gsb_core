b5b60fd45602a9b4face74c122227e2a5f400d40
Jennifer Geiß <jennifer.geiss@adesso.de>
2026-01-08 20:59:37 +0100

    fix(accessibility): add is_string condition to ternary operator
    Refs: ITZBUNDPHP-6382

--- Classes/UserFunc/ExternalLinkModifier.php
+++ Classes/UserFunc/ExternalLinkModifier.php
@@ -42,7 +42,7 @@ class ExternalLinkModifier
 
         $result = $dom->saveHTML();
 
-        return trim($result ? $result : '');
+        return trim(is_string($result) ? $result : '');
     }
 
     /**
